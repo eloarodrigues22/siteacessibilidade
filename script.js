5ab1954f36240edd855432e7351acb7f1eaf6bac
eloarodrigues22 <148449005+eloarodrigues22@users.noreply.github.com>
2025-04-10 16:22:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -39,3 +39,5 @@ document.addEventListener('DOMContentLoaded', function(){
  
  ScrollReveal().reveal('#inicio', { delay: 500 });
  ScrollReveal().reveal('#tropicalia', { delay: 500 });
+ ScrollReveal().reveal('#galeria', { delay: 500 });
+ ScrollReveal().reveal('#contato', { delay: 500 });
